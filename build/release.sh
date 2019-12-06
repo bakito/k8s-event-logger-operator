879e9b2a051a69e304d1bd3d37c33af632b6d07f
bakito <github@bakito.ch>
2019-12-06 07:58:35 +0100

    create release on github

--- build/release.sh
+++ build/release.sh
@@ -2,24 +2,36 @@
 
 set -e
 
-
 CHANGED=$(git diff-index --name-only HEAD --)
 if [[ ! -z $CHANGED ]]; then
     echo "Please commit your local changes first"
     exit 1
 fi
 
-sed -i "s/Version = \".*\"/Version = \"${1}\"/" version/version.go
-sed -i "s/version: .*/version: ${1}/" helm/Chart.yaml
-sed -i "s/appVersion: .*/appVersion: v${1}/" helm/Chart.yaml
+RELEASE=${1}
+
+sed -i "s/Version = \".*\"/Version = \"${RELEASE}\"/" version/version.go
+sed -i "s/version: .*/version: ${RELEASE}/" helm/Chart.yaml
+sed -i "s/appVersion: .*/appVersion: v${RELEASE}/" helm/Chart.yaml
 operator-sdk generate openapi
 operator-sdk generate k8s
 
 cp deploy/crds/*crd.yaml helm/crds/
 
 git add . 
-git commit -m "prepare release ${1}"
+git commit -m "prepare release ${RELEASE}"
 git push
 
-git tag -a v${1} -m "release ${1}"
-git push origin v${1}
+git tag -a v${RELEASE} -m "release ${RELEASE}"
+git push origin v${RELEASE}
+
+curl --header "Content-Type: application/json" \
+  --header "Authorization: token ${GITHUB_TOKEN}}" \
+  --request POST \
+  --data "{
+  \"tag_name\": \"v${RELEASE}\",
+  \"name\": \"v${RELEASE}\",
+  \"body\": \"Release v${RELEASE}\",
+  \"draft\": false,
+  \"prerelease\": false
+}" https://api.github.com/repos/bakito/k8s-event-logger-operator/releases
