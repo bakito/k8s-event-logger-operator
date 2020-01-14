d086203aebea5a6bc1761c43177444013620a673
bakito <github@bakito.ch>
2020-01-14 20:28:03 +0100

    push

--- build/push-to-quay.sh
+++ build/push-to-quay.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 echo "$QUAY_BOT_PASSWORD" | docker login -u "$QUAY_BOT_USERNAME" --password-stdin quay.io
-docker push ${1}
+
+
+for img in "$@"; do
+  if [ -z "${TRAVIS_TAG}" ]; then
+    docker ${img}:${TRAVIS_BRANCH} ${img}:${TRAVIS_TAG}
+    docker ${img}:${TRAVIS_BRANCH} ${img}:latest
+    docker push ${img}:${TRAVIS_TAG}
+    docker push ${img}:latest
+  else
+    docker push ${img}:${TRAVIS_BRANCH}
+  fi
+done
+
+
